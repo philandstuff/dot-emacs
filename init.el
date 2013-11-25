cc6600d5e001e46375dc3df7da094fff82eb3c0d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-11-25 14:20:45 +0000

    Specify org-mode capture file

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
 ;; org mode shortcuts
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
+(setq org-default-notes-file (concat org-directory "/notes.org"))
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 ;; magit-status
