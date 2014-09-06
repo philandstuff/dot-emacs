aa6cddd109f6fc807b469fb11a28e298886d063d
Philip Potter <philip.g.potter@gmail.com>
2014-09-06 08:46:27 +0100

    check custom.el into source control
    There are things in here I want to keep, like the rcirc 2000-line buffer
    limit

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(geiser-racket-binary "~/racket/bin/racket")
+ '(markdown-command "kramdown")
+ '(org-agenda-files (quote ("~/org/todo.org")))
+ '(org-ditaa-jar-path "~/bin/ditaa.jar")
+ '(rcirc-buffer-maximum-lines 2000))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
